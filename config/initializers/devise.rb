e3f58493d0ef5735d5a8ce4d7728b488db882817
Chazz Michaels <michaelchasedev@gmail.com>
2018-07-07 20:29:22 -0400

    create Admin for devise, update some views for sign in, new, edit, _links, update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '9894823eeb47fcf4bd91e2ba38372931961443fea2e05b8218401d7a255a196a2f232f328e6aab6d84ad3766f086947a58767a1efc082e9f5b5b0033d3836594'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
